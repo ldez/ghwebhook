d5e4af8cc9697a57646b4554d225da4d51c75d04
Fernandez Ludovic <lfernandez.dev@gmail.com>
2020-03-21 12:46:22 +0100

    chore: update dependencies.

--- event_handlers.go
+++ event_handlers.go
@@ -3,7 +3,7 @@ package ghwebhook
 import (
 	"net/url"
 
-	"github.com/google/go-github/v29/github"
+	"github.com/google/go-github/v30/github"
 )
 
 // EventHandlers all event handlers
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ldez/ghwebhook/v2
 
 go 1.13
 
-require github.com/google/go-github/v29 v29.0.2
+require github.com/google/go-github/v30 v30.0.0
